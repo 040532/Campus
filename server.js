092c0123363d02c97f5c74fee6c6ed7bb9e63f39
Moon Patel <moonpatel2003@gmail.com>
2022-11-14 19:10:24 +0530

    login, register, dashboard pages connected

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const EmployeeRoute = require("./routes/Emproutes");
+const { authenticate } = require("./controllers/EmpController");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,9 +19,20 @@ db.once("open", () => {
 });
 
 app.use(morgan("dev"));
-app.use(cookieParser('secret'));
+app.use(cookieParser("secret"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const root = "./public";
+app.get("/login", (req, res, next) => {
+    res.sendFile("login.html", { root });
+})
+app.get("/register", (req, res, next) => {
+    res.sendFile("registration.html", { root });
+});
+app.get("/dashboard", (req, res, next) => {
+    res.sendFile("dashboard.html", { root });
+});
 
 app.use("/api/employee", EmployeeRoute);
 
