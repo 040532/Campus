7fbc159e2591450124dae15b837860ea1cfdaca8
Moon Patel <moonpatel2003@gmail.com>
2022-11-17 14:37:58 +0530

    serving statics files

---
+++ public/js/demo.js
@@ -0,0 +1,29 @@
+function displayText() {
+    document.querySelector(".pts-text").innerHTML = "Points";
+  }
+  
+function countTo(){
+    let from = 0;
+    let to = 79;
+    let step = to > from ? 1 : -1;
+    let interval = 50;
+
+    if(from == to){
+        document.querySelector(".val").textContent = from;
+        return;
+    }
+
+    let counter = setInterval(function(){
+        if(from==0){
+            displayText();
+        }
+        from += step;
+        document.querySelector(".val").textContent = from;
+
+        if(from == to){
+            clearInterval(counter);
+        }
+    }, interval);
+}
+countTo();
+
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { authenticate } = require("./controllers/EmpController");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://localhost:27017/testdb");
+mongoose.connect("mongodb://127.0.0.1:27017/testdb");
 const db = mongoose.connection;
 db.on("error", (err) => {
     console.log(err);
@@ -18,12 +18,13 @@ db.once("open", () => {
     console.log("Database Connection Established!");
 });
 
+app.use(express.static("./public"));
 app.use(morgan("dev"));
 app.use(cookieParser("secret"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const root = "./public";
+const root = "./views";
 app.get("/login", (req, res, next) => {
     res.sendFile("login.html", { root });
 });
